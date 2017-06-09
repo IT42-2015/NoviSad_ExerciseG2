aa901bb3e6569a85057070d142563ad83ce31689
somename <someemail>
2017-06-09 12:09:44 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_rotate_right() {
+	public void test_rotate_right_one_time() {
 		PlanetExplorer pe = new PlanetExplorer(3,3,null);
 		
 		assertEquals("Greska", "(0,0,e)", pe.executeCommand("r"));
+	}
+	
+	
+	@Test
+	public void test_rotate_right_two_times() {
+		PlanetExplorer pe = new PlanetExplorer(3,3,null);
+		
+		pe.executeCommand("r");
+		assertEquals("Greska", "(0,0,s)", pe.executeCommand("r"));
 	}
 }
