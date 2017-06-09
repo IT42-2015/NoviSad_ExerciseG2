0dfa52caa06d7bfb88f59672ebd976654d4f2265
somename <someemail>
2017-06-09 12:02:29 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,6 +8,6 @@ public class TestPlanetExplorer {
 	public void test_rotate_right() {
 		PlanetExplorer pe = new PlanetExplorer(3,3,null);
 		
-		assertEquals("Greska", "(0,0,n)", pe.executeCommand("r"));
+		assertEquals("Greska", "(0,0,e)", pe.executeCommand("r"));
 	}
 }
